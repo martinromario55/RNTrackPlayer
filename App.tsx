207562e8535617eb02141a20a08d74880367d899
martinromario55 <martinromario55@gmail.com>
2024-03-19 08:33:47 +0300

    Player setup in App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,54 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {addTrack, setupPlayer} from './musicPlayerServices';
 
 const App = () => {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  const setup = async () => {
+    // Get the true or false result from setupPlayer Service
+    let isSetup = await setupPlayer();
+
+    // If track is available, then add it
+    if (isSetup) {
+      await addTrack();
+    }
+    setIsPlayerReady(isSetup);
+  };
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  // If player is not ready, show loading icon
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <Text>App</Text>
-    </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
 
 export default App;
